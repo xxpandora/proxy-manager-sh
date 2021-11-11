d012895566c989ba8dea73cab407ad11729b4110
Pandora <ykalbayrak53@gmail.com>
2021-11-11 14:41:47 +0300

    new version script 2.9.12

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+TMP=/tmp/pegacdn_install.sh
+URL=https://raw.githubusercontent.com/xxpandora/proxmox-scripts/main/lxc/nginx-proxy-manager/install
+
+if [ "$(uname)" != "Linux" ]; then
+  echo "OS NOT SUPPORTED"
+  exit 1
+fi
+
+DISTRO=$(cat /etc/*-release | grep -w ID | cut -d= -f2 | tr -d '"')
+if [ "$DISTRO" != "alpine" ] && [ "$DISTRO" != "ubuntu" ]; then
+  echo "DISTRO NOT SUPPORTED"
+  exit 1
+fi
+
+rm -rf $TMP
+wget -O "$TMP" "$URL/$DISTRO.sh"
+
+chmod +x "$TMP"
+
+if [ "$(command -v bash)" ]; then
+  bash "$TMP"
+else
+  sh "$TMP"
+fi
+
