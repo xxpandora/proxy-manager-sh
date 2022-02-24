a60171d0201ab136f8cfa1fc95624f007ff93a21
Pandora <ykalbayrak53@gmail.com>
2022-02-25 00:04:31 +0300

    update

--- setup_v3.sh
+++ setup_v3.sh
@@ -2,21 +2,10 @@
 TMP=/tmp/pegacdn_install.sh
 URL=https://raw.githubusercontent.com/xxpandora/proxy-manager-sh/main/install
 
-if [ "$(uname)" != "Linux" ]; then
-  echo "OS NOT SUPPORTED"
-  exit 1
-fi
-
-DISTRO=$(cat /etc/*-release | grep -w ID | cut -d= -f2 | tr -d '"')
-if [ "$DISTRO" != "alpine" ] && [ "$DISTRO" != "ubuntu" ] && [ "$DISTRO" != "debian" ]; then
-  echo "DISTRO NOT SUPPORTED"
-  exit 1
-fi
-
-INSTALL_SCRIPT=$DISTRO
+INSTALL_SCRIPT=ubuntu_v3
 
 rm -rf $TMP
-wget -O "$TMP" "$URL/$INSTALL_SCRIPT_v3.sh"
+wget -O "$TMP" "$URL/$INSTALL_SCRIPT.sh"
 
 chmod +x "$TMP"
 
