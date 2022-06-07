45b8d8cdd17852bfe2513729d0a2de9bc1e5127d
pandora <yahya@yahyakadir.name.tr>
2022-06-07 12:12:31 +0300

    update v4

---
+++ setup_v4.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+TMP=/tmp/pegacdn_install.sh
+URL=https://raw.githubusercontent.com/xxpandora/proxy-manager-sh/main/install
+
+INSTALL_SCRIPT=ubuntu_v3
+
+rm -rf $TMP
+wget -O "$TMP" "$URL/$INSTALL_SCRIPT.sh"
+
+chmod +x "$TMP"
+
+if [ "$(command -v bash)" ]; then
+  $(command -v sudo) bash "$TMP"
+else
+  sh "$TMP"
+fi
+
