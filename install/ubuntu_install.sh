049e4438f47998f8eef31b3f017f99779a730858
pandora <ykalbayrak53@gmail.com>
2023-01-15 15:55:44 +0300

    Update ubuntu_install.sh

--- install/ubuntu_install.sh
+++ install/ubuntu_install.sh
@@ -65,14 +65,11 @@ runcmd pip install --no-cache-dir cffi certbot
 
 # Install openresty
 log "Installing openresty"
-wget -qO - https://openresty.org/package/pubkey.gpg | apt-key add -
-_distro_release=$(wget $WGETOPT "http://openresty.org/package/$DISTRO_ID/dists/" -O - | grep -o "$DISTRO_CODENAME" | head -n1 || true)
-if [ $DISTRO_ID = "ubuntu" ]; then
-  echo "deb [trusted=yes] http://openresty.org/package/$DISTRO_ID ${_distro_release:-focal} main" | tee /etc/apt/sources.list.d/openresty.list
-else
-  echo "deb [trusted=yes] http://openresty.org/package/$DISTRO_ID ${_distro_release:-bullseye} openresty" | tee /etc/apt/sources.list.d/openresty.list
-fi
-runcmd apt-get update && apt-get install -y -q --no-install-recommends openresty
+runcmd wget -qO - https://openresty.org/package/pubkey.gpg | apt-key add -
+runcmd apt-get -y install software-properties-common
+runcmd add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
+runcmd apt-get update -y
+rumcmd sudo apt-get install openresty
 
 # Install nodejs
 log "Installing nodejs"
